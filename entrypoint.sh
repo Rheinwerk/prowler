1ce9f762630db8426900e84f760db1d3bb6d5f17
Michael Schmitz <s_michael1@gmx.de>
2022-12-19 13:37:38 +0100

    [#183246166] Modify Prowler Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-
 set -Eeuo pipefail
+
+die() {
+    echo "$1"
+    exit 1
+}
+
+test -z $PROWLER_RULES_EXCLUDED && die "PROWLER_RULES_EXCLUDED not set!"
+test -z $AWS_REGION && die "AWS_REGION not set!"
+test -z $PROWLER_OUTPUT_FORMATS && die "PROWLER_OUTPUT_FORMATS not set!"
+test -z $PROWLER_REPORT_BUCKET && die "PROWLER_REPORT_BUCKET not set!"
+test -z $PROWLER_WHITELIST && die "PROWLER_WHITELIST not set!"
+
+
 printenv
-/prowler/prowler -V
+/prowler/prowler -E "${PROWLER_RULES_EXCLUDED}" -r "${AWS_REGION}" -f "${AWS_REGION}" -M "${PROWLER_OUTPUT_FORMATS}" -D "${PROWLER_REPORT_BUCKET}" -w "${PROWLER_WHITELIST}"
