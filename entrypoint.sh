20af55935fd47e072bf6ae2e06f413e44d965a78
Michael Schmitz <s_michael1@gmx.de>
2022-12-19 14:15:09 +0100

    [#183246166] Modify Prowler Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+# initialize Exitcode variable
+EXITCODE=0
+
 die() {
     echo "$1"
     exit 1
@@ -12,6 +15,11 @@ test -z $PROWLER_OUTPUT_FORMATS && die "PROWLER_OUTPUT_FORMATS not set!"
 test -z $PROWLER_REPORT_BUCKET_PATH && die "PROWLER_REPORT_BUCKET_PATH not set!"
 test -z $PROWLER_WHITELIST && die "PROWLER_WHITELIST not set!"
 
-
+# print out environment variables for debug
 printenv
-/prowler/prowler -E "${PROWLER_RULES_EXCLUDED}" -r "${AWS_REGION}" -f "${AWS_REGION}" -M "${PROWLER_OUTPUT_FORMATS}" -D "${PROWLER_REPORT_BUCKET_PATH}" -w "${PROWLER_WHITELIST}"
+
+# run prowler
+/prowler/prowler -E "${PROWLER_RULES_EXCLUDED}" -r "${AWS_REGION}" -f "${AWS_REGION}" -M "${PROWLER_OUTPUT_FORMATS}" -D "${PROWLER_REPORT_BUCKET_PATH}" -w "${PROWLER_WHITELIST}" || EXITCODE=$?
+
+# Exit with 0 if successful, otherwise with return code from prowler command
+exit "$EXITCODE"
